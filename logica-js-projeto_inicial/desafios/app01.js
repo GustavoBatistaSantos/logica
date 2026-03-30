bf671fd396ddd820ccbe5d95ea69ce6636ff0c99
Gustavo Batista <145714578+gustavo1506@users.noreply.github.com>
2026-03-30 18:46:32 -0300

    sd

---
+++ logica-js-projeto_inicial/desafios/app01.js
@@ -0,0 +1,11 @@
+alert('Boas vindas ao nosso site');
+let nome = "lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = alert ("Erro!Preencha todos os campos")
+let nome2 = prompt("Qual é o seu nome?")
+let idade2 = prompt("Qual é a sua idade?")
+if (idade2>=18){
+    alert("Pode tirar a habilitação!")
+}
