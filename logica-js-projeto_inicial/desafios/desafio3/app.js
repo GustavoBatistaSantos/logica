ef3242aebf85b7f86c2c699254f59ff5254cdadb
Gustavo Batista <145714578+gustavo1506@users.noreply.github.com>
2026-04-02 15:40:48 -0300

    kjk

---
+++ logica-js-projeto_inicial/desafios/desafio3/app.js
@@ -0,0 +1,20 @@
+/*var numero = 1
+var n2 = 10
+var n3 = prompt("digite um número")*/
+var n4 = prompt("digite um número")
+n5=0
+
+/*while(numero<11){
+    console.log(`numero: ${numero}`)
+    numero++
+}
+
+while(n2>=0){
+    console.log(`numero: ${n2}`)
+    n2--
+}*/
+
+while (n4>=n5){
+    console.log(`o próximo número é ${n5}`)
+    n5++
+}
